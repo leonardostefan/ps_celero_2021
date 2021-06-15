b542c91b9106765de27c087bca2e39086d2cac5c
leonardostefan <leonardostefan@hotmail.com>
2021-06-15 20:16:04 -0300

    rotas para o modelo 'atleta'

--- ps_celero/api/serializers.py
+++ ps_celero/api/serializers.py
@@ -2,38 +2,31 @@ from rest_framework import serializers
 from .models import *
 
 
-class AthleteSerializaer(serializers.Model):
+class AthleteSerializaer(serializers.ModelSerializer):
     class Meta:
         model = Athlete
         fields = ['id', 'name', 'sex', 'birth_year']
 
 
-class GamesSerializaer(serializers.Model):
+class GamesSerializaer(serializers.ModelSerializer):
     class Meta:
         model = Games
         fields = ['games', 'season', 'year', 'city']
 
 
-class EventSerializaer(serializers.Model):
+class EventSerializaer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = ['id', 'event', 'sport', 'gameId']
 
 
-class NocSerializaer(serializers.Model):
+class NocSerializaer(serializers.ModelSerializer):
     class Meta:
         model = Noc
         fields = ['acronym', 'region', 'notes']
 
-
-class TeamSerializaer (serializers.Model):
-    class Meta:
-        model = Team
-        fields = ['team', 'nocId']
-
-
-class AthleteEventStatSerializaer(serializers.Model):
+class AthleteEventStatSerializaer(serializers.ModelSerializer):
     class Meta:
         model = AthleteEventStat
         fields = ['id', 'atlheteId', 'height', 'weight',
-                  'eventId', 'teamId', 'medal']
+                  'eventId', 'team', 'medal']
